c92fd51d9ab6519fb8f9164964f729180efdfb34
Hao Wang <49464125+ChillTran@users.noreply.github.com>
2024-01-17 14:57:14 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://whcolony.com/", // replace this with your deployed domain
+  author: "Hao Wang",
+  desc: "A place where he is placed.",
+  title: "whcolony",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
